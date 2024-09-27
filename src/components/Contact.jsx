c5119bcdde0908155c60497e266da0032baf26dc
pradipbhatt <bhattsammar04@gmail.com>
2024-09-27 20:50:02 +0545

    Responsive make UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,48 +16,47 @@ export default function ContactSection() {
   };
 
   return (
-    <div className="bg-gradient-to-r from-[rgba(4,81,97,0.5)] via-[rgba(4,81,97,0.3)] to-[rgba(4,81,97,0)] p-12 sm:flex sm:justify-between">
+    <div className="bg-gradient-to-r from-[rgba(4,81,97,0.5)] via-[rgba(4,81,97,0.3)] to-[rgba(4,81,97,0)] p-2 lg:p-12 sm:flex sm:justify-between">
       {/* Contact Info Section */}
-      <div className="bg-transparent p-14 sm:w-1/2 flex flex-col justify-between">
-  <h2 className="text-4xl font-bold text-gray-800 mb-2">CONTACT</h2> {/* Reduced margin */}
-  <p className="text-gray-900 mb-8 leading-relaxed">
-    We believe in conversations. Whether you have a business proposition,
-    a query, or simply wish to learn more about what we do, our doors
-    (and inboxes) are always open. With a team of dedicated professionals
-    ready to assist, your inquiries are always in expert hands.
-  </p>
+      <div className="bg-transparent p-2 lg:p-12 sm:p-4  sm:p-4 sm:w-1/2 flex flex-col justify-between">
+        <h2 className="text-4xl font-bold text-gray-800 mb-2">CONTACT</h2>
+        <p className="text-gray-900 mb-8 leading-relaxed">
+          We believe in conversations. Whether you have a business proposition,
+          a query, or simply wish to learn more about what we do, our doors
+          (and inboxes) are always open. With a team of dedicated professionals
+          ready to assist, your inquiries are always in expert hands.
+        </p>
 
-  <div className="space-y-4">
-    <p className="text-gray-700 flex items-center mb-2">
-      <img src={location} alt="Location Icon" className="w-8 h-8 mr-4" />
-      Riyadh, Saudi Arabia
-    </p>
-    <p className="text-gray-700 flex items-center mb-2">
-      <img src={mail} alt="Mail Icon" className="w-8 h-8 mr-4" />
-      info@emkaan.sa
-    </p>
-    <p className="text-gray-700 flex items-center mb-6">
-      <img src={phone} alt="Phone Icon" className="w-8 h-8 mr-4" />
-      +966 500487676
-    </p>
+        <div className="space-y-4">
+          <p className="text-gray-700 flex items-center mb-2">
+            <img src={location} alt="Location Icon" className="w-8 h-8 mr-4" />
+            Riyadh, Saudi Arabia
+          </p>
+          <p className="text-gray-700 flex items-center mb-2">
+            <img src={mail} alt="Mail Icon" className="w-8 h-8 mr-4" />
+            info@emkaan.sa
+          </p>
+          <p className="text-gray-700 flex items-center mb-6">
+            <img src={phone} alt="Phone Icon" className="w-8 h-8 mr-4" />
+            +966 500487676
+          </p>
 
-    <div className="flex space-x-4 mt-4"> {/* Added margin top for links */}
-      <a
-        href="#"
-        className="flex items-center justify-center w-8 h-8 rounded-full transition-all duration-1000 ease-in-out hover:bg-gradient-to-r hover:from-teal-800 hover:to-teal-400"
-      >
-        <img src={linkedin} alt="LinkedIn Icon" className="w-8 h-8" />
-      </a>
-      <a
-        href="#"
-        className="flex items-center justify-center w-9 h-9 rounded-full transition-all duration-1000 ease-in-out hover:bg-gradient-to-r hover:from-teal-800 hover:to-teal-400"
-      >
-        <img src={twitter} alt="Twitter Icon" className="w-12 h-12" />
-      </a>
-    </div>
-  </div>
-</div>
-
+          <div className="flex space-x-4 mt-4">
+            <a
+              href="#"
+              className="flex items-center justify-center w-8 h-8 rounded-full transition-all duration-1000 ease-in-out hover:bg-gradient-to-r hover:from-teal-800 hover:to-teal-400"
+            >
+              <img src={linkedin} alt="LinkedIn Icon" className="w-8 h-8" />
+            </a>
+            <a
+              href="#"
+              className="flex items-center justify-center w-9 h-9 rounded-full transition-all duration-1000 ease-in-out hover:bg-gradient-to-r hover:from-teal-800 hover:to-teal-400"
+            >
+              <img src={twitter} alt="Twitter Icon" className="w-12 h-12" />
+            </a>
+          </div>
+        </div>
+      </div>
 
       {/* Form Section */}
       <div className="bg-white bg-opacity-80 p-8 rounded-md shadow-lg w-full sm:w-1/2 mt-10 sm:mt-0" style={{ fontFamily: "Roboto, sans-serif" }}>
@@ -79,7 +78,7 @@ export default function ContactSection() {
               </svg>
             </div>
             <h3 className="text-2xl font-semibold mb-2 tracking-wider">Message Received Successfully!</h3>
-<p className="text-gray-700 mb-4 tracking-widest">Thanks for your message, we will get back to you as soon as possible.</p>
+            <p className="text-gray-700 mb-4 tracking-widest">Thanks for your message, we will get back to you as soon as possible.</p>
 
             <button
               onClick={() => setSubmitted(false)} // Reset form on button click
@@ -122,12 +121,11 @@ export default function ContactSection() {
               ></textarea>
             </div>
             <button
-  type="submit"
-  className="w-full p-2 bg-[#04516180] text-white font-bold rounded-md shadow-md transition-colors duration-1400 ease-in-out hover:bg-gradient-to-r hover:from-teal-800 hover:to-blue-200"
->
-  SEND ➔
-</button>
-
+              type="submit"
+              className="w-full p-2 bg-[#04516180] text-white font-bold rounded-md shadow-md transition-colors duration-1400 ease-in-out hover:bg-gradient-to-r hover:from-teal-800 hover:to-blue-200"
+            >
+              SEND ➔
+            </button>
           </form>
         )}
       </div>
